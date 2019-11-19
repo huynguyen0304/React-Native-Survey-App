9cd7978c3887e5719355e799e6ddb8247763d7b0
huynguyen0304 <huyaxelo0304@gmail.com>
2019-11-19 18:41:05 +0700

    update result: lay answer

--- src/Components/Main/Results.js
+++ src/Components/Main/Results.js
@@ -1,69 +1,124 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, ActivityIndicator, FlatList } from 'react-native';
+import { Text, StyleSheet, View, ActivityIndicator, FlatList, ScrollView } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
-import question from '../../API/question';
+
+
 
 export default class Results extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dataSource: [],
-            id_question: "",
-            questions: [],
-            answers: [],
             results: [],
             isLoading: true
         }
     }
 
-    renderItem = ({ item }) => {
-        const questions = this.state.dataSource.map((item, index) => {
-            return item.questions//.map((item, index) => {
-            //return item.title;
-            //});
-        });
-        const questionTile = questions.map((items, index) => {
-            return items.map(item => item.title)
-        });
-        // alert(...questionTile);
-
-        /**
-         * const list = this.state.data.map((item, index) => {
-              return item.questions.map((item, index) => {
-                return <h1 key={index}>{item.title}</h1>;
-              });
-            });
-         */
-
+    data = ({ item }) => {
         return (
-            <View style={styles.rowItem}>
-                <Text>{questionTile}</Text>
-                {/* <PieChart
-                    data="{results}"
-                    width="{screenWidth}"
-                    height="{220}"
-                    chartConfig="{chartConfig}"
-                    backgroundColor="transparent"
-                    paddingLeft="15"
-                /> */}
+            <View>
+                <Text>{item.answer}</Text>
             </View>
         )
     }
 
+    value = ({ item }) => {
+        return (
+            <View>
+                <Text>{item.value}</Text>
+            </View>
+        )
+    }
+
+    renderItem = ({ item }) => {
+        if (item.type_questions === "multiple") {
+            return (
+                <View style={styles.rowItem}>
+                    <View style={styles.rowQuestion}>
+                        <Text>{item.title}</Text>
+                        <FlatList
+                            data={item.answers}
+                            renderItem={this.data}
+                            keyExtractor={item => item.id}
+                        />
+                    </View>
+                    <View style={styles.rowReport}>
+                        <Text>a</Text>
+                    </View>
+                </View>
+            )
+        } else if (item.type_questions === "yes/no") {
+            return (
+                <View style={styles.rowItem}>
+                    <View style={styles.rowQuestion}>
+                        <Text>{item.title}</Text>
+                        <FlatList
+                            data={item.answers}
+                            renderItem={this.data}
+                            keyExtractor={item => item.id}
+                        />
+                    </View>
+                    <View style={styles.rowReport}>
+                        <Text>a</Text>
+                    </View>
+                </View>
+            )
+        } else if (item.type_questions === "vote") {
+            return (
+                <View style={styles.rowItem}>
+                    <View style={styles.rowQuestion}>
+                        <Text>{item.title}</Text>
+                        <FlatList
+                            data={item.answers}
+                            renderItem={this.data}
+                            keyExtractor={item => item.id}
+                        />
+                    </View>
+                    <View style={styles.rowReport}>
+                        <Text>a</Text>
+                    </View>
+                </View>
+            )
+        } else {
+            return (
+                <View>
+                    <View style={styles.rowQuestion}>
+                        <Text>{item.title}</Text>
+                        <FlatList
+                            data={this.state.results}
+                            renderItem={this.value}
+                            keyExtractor={item => item.id}
+                        />
+                    </View>
+                </View>
+            )
+        }
+    }
+
     componentDidMount() {
-        fetch("http://my-json-server.typicode.com/huynguyen0304/Survey/db")
+        const itemid = JSON.stringify(this.props.navigation.getParam("itemid", "alooo"));
+
+        fetch("http://my-json-server.typicode.com/huynguyen0304/Survey/form/" + itemid)
             .then((res) => res.json())
             .then((responseJson) => {
                 this.setState({
-                    dataSource: responseJson.form,
+                    dataSource: responseJson.questions,
                     isLoading: false
                 })
+            });
 
-            })
+        fetch("http://my-json-server.typicode.com/huynguyen0304/Survey/evaluation/?form=" + itemid)
+            .then((res) => res.json())
+            .then((responseJson) => {
+                this.setState({
+                    results: responseJson.results,
+                    isLoading: false
+                })
+            });
     }
 
     render() {
-        const itemid = JSON.stringify(this.props.navigation.getParam("itemid", "alooo"))
+        const itemid = JSON.stringify(this.props.navigation.getParam("itemid", "alooo"));
         var list = this.state.dataSource.filter(x => x.id === itemid);
         const result = this.state.isLoading ?
             <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -71,9 +126,13 @@ export default class Results extends Component {
             </View>
             :
             <View>
+                <View style={styles.header}>
+                    <Text style={styles.headerTitle}>Results</Text>
+                </View>
                 <FlatList
                     data={this.state.dataSource}
                     renderItem={this.renderItem}
+                    keyExtractor={item => item.id}
                 />
             </View>;
 
@@ -89,16 +148,43 @@ export default class Results extends Component {
         return (
             <View>
                 {indexJSX}
-            </View>
+            </View >
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
     rowItem: {
+        justifyContent: "space-between",
+        flexDirection: "row"
+    },
+    rowQuestion: {
+        borderColor: "#000000",
+        borderWidth: 2,
+        margin: "2%",
+        padding: "1%",
+        width: "40%"
+    },
+    rowReport: {
         borderColor: "#000000",
         borderWidth: 2,
         margin: "2%",
         padding: "1%"
-    }
+    },
+    header: {
+        backgroundColor: '#1c9ad6',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        paddingVertical: "5%"
+    },
+    headerTitle: {
+        fontFamily: 'Avenir',
+        color: '#fff', fontSize: 30,
+        marginLeft: '3%',
+        fontWeight: 'bold'
+    },
 })
